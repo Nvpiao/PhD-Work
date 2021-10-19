f4b28ae2fc1dd0174d4f0829caa80665badebc7c
Nvpiao <nviao@gmail.com>
2021-10-19 17:19:06 +0800

    add class Batch

--- 12-10-2021/GAN-BERT-TRANSFORMER/learn_transformer.py
+++ 12-10-2021/GAN-BERT-TRANSFORMER/learn_transformer.py
@@ -253,3 +253,27 @@ def make_model(src_vocab, tgt_vocab, N=6,
         if p.dim() > 1:
             nn.init.xavier_uniform(p)
     return model
+
+
+# temp_model = make_model(10, 10, 2)
+# print(temp_model)
+
+
+class Batch:
+    def __init__(self, src, trg=None, pad=0):
+        self.src = src
+        self.src_mask = (src != pad).unsqueeze(-2)
+        if trg is not None:
+            self.trg = trg[:, :-1]
+            self.trg_y = trg[:, 1:]
+            self.trg_mask = \
+                self.make_std_mask(self.trg, pad)
+            self.ntokens = (self.trg_y != pad).data.sum()
+
+    @staticmethod
+    def make_std_mask(tgt, pad):
+        tgt_mask = (tgt != pad).unsqueeze(-2)
+        tgt_mask = tgt_mask & Variable(
+            subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data)
+        )
+        return tgt_mask
